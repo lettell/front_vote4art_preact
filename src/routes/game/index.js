e73918636254574d659a092b37fb4fc88335a49a
Rimantas <belauzas@yahoo.com>
2019-05-05 17:13:17 +0300

    Game page main layout #5

--- src/routes/game/index.js
+++ src/routes/game/index.js
@@ -1,14 +1,14 @@
 import { h, Component } from 'preact';
 import Card from 'preact-material-components/Card';
 import 'preact-material-components/Card/style.css';
-import 'preact-material-components/Button/style.css';
 import style from './style';
 
 export default class Game extends Component {
 	render() {
 		return (
-			<div class={`${style.home} page`}>
-				<h1>Game route</h1>
+			<div class={`${style.game} page`}>
+				<div class={style.mapCanvas}>MAP CANVAS</div>
+				<div class={style.pixel}>+</div>
 			</div>
 		);
 	}
