f827e7874e9d625e9934388cf9c9a224b01ccfda
lettell <lettell@DESKTOP-BLB8T63.home>
2019-05-11 16:10:11 +0300

    base_path to api

--- src/utils/vote4art-api.js
+++ src/utils/vote4art-api.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 import { getAccessToken } from './auth-service';
 
 // local
-const BASE_URL = 'http://localhost:3000/api/v1/';
+// const BASE_URL = 'http://localhost:3000/api/v1/';
 
 // production
-// const BASE_URL = 'http://api.vote4art.eu';
+const BASE_URL = 'http://api.vote4art.eu';
 
 export { getPixels, postPixel };
 
