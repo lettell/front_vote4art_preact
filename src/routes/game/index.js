2e9618ccf8bb3e7ddc7e65f533e50c31e928fa43
lettell <lettell@localhost.localdomain>
2019-05-19 22:11:38 +0300

    fix

--- src/routes/game/index.js
+++ src/routes/game/index.js
@@ -4,41 +4,22 @@ import Board from '../../components/board';
 
 export default class Game extends Component {
 	constructor() {
-		super()
+		super();
 		this.userStata = this.resolveState();
 		
 	}
 	resolveState() {
 		let state;
-		if (typeof window !== "undefined") { 		
+		if (typeof window !== 'undefined') {
 			state = localStorage.userState;
 		}
 		else {
 			state = -1;
 		}
 		switch (state) {
-			case 0:
-			case '0': {
-
-				return this.setState({ userState: 'visited' });
-			}
-			case 1:
-			case '1': {
-				return this.setState({ userState: 'auhenticate' });
-			}
-			case 2:
-			case '2': {
-				return this.setState({ userState: 'active' });
-			}
-			case 3:
-			case '3': {
-				return this.setState({ userState: 'disconected' });
-			}
 			default: {
-				if (typeof window !== "undefined") { 
-					this.dialog.MDComponent.show();
-
-					localStorage.setItem('userState', 0)
+				if (typeof window !== 'undefined') {
+					localStorage.setItem('userState', 0);
 				 }
 
 				return	this.setState({ dialogContent: 'game' });
@@ -46,7 +27,7 @@ export default class Game extends Component {
 			}
 		}
 	}
-	render({x, y, zoom}) {
+	render({ x, y, zoom }) {
 		return (
 			 <div class="container_main">
 				<Board x={x} y={y} zoom={zoom} />
