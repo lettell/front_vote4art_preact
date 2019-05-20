51b5ee90d9a66bd5088b430a07be94b1d9aa5fd2
lettell <lettell@localhost.localdomain>
2019-05-20 10:55:39 +0300

    fix

--- src/components/board/index.js
+++ src/components/board/index.js
@@ -103,7 +103,7 @@ export default class Board extends Component {
 			this.setState({ activePixels: 'loaded' });
 			if (resp.data && resp.data.length){
 				this.setAllPixels(resp.data);
-				const photo = `https://nuotraukos.vote4art.eu${resp.meta.photo}`;
+				const photo = `https://nuotraukos.vote4art.eu/${resp.meta.photo}`;
 				if (this.state.currentPhoto !== photo ){
 					this.setState({ photoUpdate: 'done', currentPhoto: photo });
 				}
