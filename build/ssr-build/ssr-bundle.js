84c5a4ba8e2ef69db1d7e9741e5c4f2d736b8857
lettell <lettell@localhost.localdomain>
2019-05-21 05:20:33 +0300

    build

--- build/ssr-build/ssr-bundle.js
+++ build/ssr-build/ssr-bundle.js
@@ -9803,10 +9803,11 @@ var lib_default = /*#__PURE__*/__webpack_require__.n(lib);
 
 var ACCESS_TOKEN_KEY = 'va',
     ID_TOKEN_KEY = 'la',
+    BASE_URL = 'https://api.vote4art.eu',
 
-// BASE_URL = 'https://api.vote4art.eu',
-BASE_URL = 'http://localhost:3000',
-    BASE_URL_PRIVATE = BASE_URL + '/api/v1';
+// BASE_URL = 'http://localhost:3000',
+
+BASE_URL_PRIVATE = BASE_URL + '/api/v1';
 // production
 
 
@@ -11022,11 +11023,11 @@ var preact_tap_event_plugin_default = /*#__PURE__*/__webpack_require__.n(preact_
 
 
 // local
-var vote4art_api_BASE_URL = 'http://localhost:3000/api/v1';
+// const BASE_URL = 'http://localhost:3000/api/v1';
 
 
 // production
-// const BASE_URL = 'https://api.vote4art.eu/api/v1';
+var vote4art_api_BASE_URL = 'https://api.vote4art.eu/api/v1';
 
 
 
@@ -31506,8 +31507,8 @@ var app_App = function (_Component) {
 	App.prototype.render = function render() {
 		console.log(this.state);
 		// document.body.classList.add('mdc-theme--main');
-		// const base = 'https://vote4art.eu/';
-		var base = 'https://192.168.0.100:8080/';
+		var base = 'https://vote4art.eu/';
+		// const base = 'https://192.168.0.100:8080/';
 		return Object(preact_min["h"])(
 			'div',
 			{ id: 'app' },
