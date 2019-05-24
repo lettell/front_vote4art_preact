5e58693278012549f29e1e95514db0e5da21cebb
lettell <lettell@localhost.localdomain>
2019-05-24 19:34:52 +0300

    ok

--- build/ssr-build/ssr-bundle.js
+++ build/ssr-build/ssr-bundle.js
@@ -10048,12 +10048,14 @@ function login(pramas) {
 	});
 }
 function signup(pramas) {
+
 	var url = BASE_URL + '/api/v1/signup';
 	return axios_default.a.post(url, {
 		username: pramas.username,
 		password: pramas.password,
 		password_confirmation: pramas.password_confirmation,
-		terms_and_conditions: pramas.terms_and_conditions
+		terms_and_conditions: pramas.terms_and_conditions,
+		finger: pramas.finger
 
 	}).then(function (response) {
 		localStorage.setItem('visited', true);
@@ -10501,6 +10503,10 @@ var Checkbox_default = /*#__PURE__*/__webpack_require__.n(Checkbox);
 var google_login = __webpack_require__("P+aA");
 var google_login_default = /*#__PURE__*/__webpack_require__.n(google_login);
 
+// EXTERNAL MODULE: ../node_modules/fingerprintjs/fingerprint.js
+var fingerprint = __webpack_require__("oNni");
+var fingerprint_default = /*#__PURE__*/__webpack_require__.n(fingerprint);
+
 // EXTERNAL MODULE: ./components/auth/registration/style.scss
 var registration_style = __webpack_require__("rxD2");
 var registration_style_default = /*#__PURE__*/__webpack_require__.n(registration_style);
@@ -10516,6 +10522,7 @@ function registration__inherits(subClass, superClass) { if (typeof superClass !=
 
 
 // import { route } from 'preact-router';
+
 
 
 
@@ -10566,6 +10573,7 @@ var registration_Registration = function (_Component) {
 
 		_this.registerSimple = function () {
 			_this.state.terms_and_conditions = _this.cb.MDComponent.checked;
+			_this.state.finger = _this.finger;
 			signup(_this.state).then(function (resp) {
 				if (resp) {
 					_this.setState({});
@@ -10650,6 +10658,15 @@ var registration_Registration = function (_Component) {
 		_this.validatePassword = _this.validatePassword.bind(_this);
 		_this.registerSimple = _this.registerSimple.bind(_this);
 
+		if (window.requestIdleCallback) {
+			requestIdleCallback(function () {
+				_this.finger = new fingerprint_default.a().get();
+			});
+		} else {
+			setTimeout(function () {
+				_this.finger = new fingerprint_default.a().get();
+			}, 500);
+		}
 		return _this;
 	}
 
@@ -31941,7 +31958,6 @@ function app__inherits(subClass, superClass) { if (typeof superClass !== "functi
 
 
 
-
 var app__ref = Object(preact_min["h"])('link', { href: 'https://fonts.googleapis.com/css?family=Exo+2', rel: 'stylesheet' });
 
 var app__ref2 = Object(preact_min["h"])('link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' });
@@ -31958,7 +31974,8 @@ var app__ref7 = Object(preact_min["h"])(
 	'noscript',
 	null,
 	Object(preact_min["h"])('img', { height: '1', width: '1',
-		src: 'https://www.facebook.com/tr?id=305085283706777&ev=PageView &noscript=1' })
+		src: 'https://www.facebook.com/tr?id=305085283706777&ev=PageView &noscript=1'
+	})
 );
 
 var app_App = function (_Component) {
@@ -32085,7 +32102,7 @@ var app_App = function (_Component) {
 	App.prototype.render = function render() {
 		// document.body.classList.add('mdc-theme--main');
 		var base = 'https://vote4art.eu/';
-		// const base = ' http://192.168.0.100:46003';
+		// const base = 'http://localhost:8080';
 
 		return Object(preact_min["h"])(
 			'div',
@@ -48408,6 +48425,281 @@ function isPrototype(value) {
 }
 
 module.exports = isPrototype;
+
+/***/ }),
+
+/***/ "oNni":
+/***/ (function(module, exports, __webpack_require__) {
+
+var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_RESULT__;/*
+* fingerprintJS 0.5.3 - Fast browser fingerprint library
+* https://github.com/Valve/fingerprintjs
+* Copyright (c) 2013 Valentin Vasilyev (valentin.vasilyev@outlook.com)
+* Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) license.
+*/
+
+;(function (name, context, definition) {
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = definition();
+  } else if (true) {
+    !(__WEBPACK_AMD_DEFINE_FACTORY__ = (definition),
+				__WEBPACK_AMD_DEFINE_RESULT__ = (typeof __WEBPACK_AMD_DEFINE_FACTORY__ === 'function' ?
+				(__WEBPACK_AMD_DEFINE_FACTORY__.call(exports, __webpack_require__, exports, module)) :
+				__WEBPACK_AMD_DEFINE_FACTORY__),
+				__WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
+  } else {
+    context[name] = definition();
+  }
+})('Fingerprint', this, function () {
+  'use strict';
+
+  var Fingerprint = function Fingerprint(options) {
+    var nativeForEach, nativeMap;
+    nativeForEach = Array.prototype.forEach;
+    nativeMap = Array.prototype.map;
+
+    this.each = function (obj, iterator, context) {
+      if (obj === null) {
+        return;
+      }
+      if (nativeForEach && obj.forEach === nativeForEach) {
+        obj.forEach(iterator, context);
+      } else if (obj.length === +obj.length) {
+        for (var i = 0, l = obj.length; i < l; i++) {
+          if (iterator.call(context, obj[i], i, obj) === {}) return;
+        }
+      } else {
+        for (var key in obj) {
+          if (obj.hasOwnProperty(key)) {
+            if (iterator.call(context, obj[key], key, obj) === {}) return;
+          }
+        }
+      }
+    };
+
+    this.map = function (obj, iterator, context) {
+      var results = [];
+      // Not using strict equality so that this acts as a
+      // shortcut to checking for `null` and `undefined`.
+      if (obj == null) return results;
+      if (nativeMap && obj.map === nativeMap) return obj.map(iterator, context);
+      this.each(obj, function (value, index, list) {
+        results[results.length] = iterator.call(context, value, index, list);
+      });
+      return results;
+    };
+
+    if (typeof options == 'object') {
+      this.hasher = options.hasher;
+      this.screen_resolution = options.screen_resolution;
+      this.canvas = options.canvas;
+      this.ie_activex = options.ie_activex;
+    } else if (typeof options == 'function') {
+      this.hasher = options;
+    }
+  };
+
+  Fingerprint.prototype = {
+    get: function get() {
+      var keys = [];
+      keys.push(navigator.userAgent);
+      keys.push(navigator.language);
+      keys.push(screen.colorDepth);
+      if (this.screen_resolution) {
+        var resolution = this.getScreenResolution();
+        if (typeof resolution !== 'undefined') {
+          // headless browsers, such as phantomjs
+          keys.push(this.getScreenResolution().join('x'));
+        }
+      }
+      keys.push(new Date().getTimezoneOffset());
+      keys.push(this.hasSessionStorage());
+      keys.push(this.hasLocalStorage());
+      keys.push(!!window.indexedDB);
+      //body might not be defined at this point or removed programmatically
+      if (document.body) {
+        keys.push(typeof document.body.addBehavior);
+      } else {
+        keys.push(typeof undefined);
+      }
+      keys.push(typeof window.openDatabase);
+      keys.push(navigator.cpuClass);
+      keys.push(navigator.platform);
+      keys.push(navigator.doNotTrack);
+      keys.push(this.getPluginsString());
+      if (this.canvas && this.isCanvasSupported()) {
+        keys.push(this.getCanvasFingerprint());
+      }
+      if (this.hasher) {
+        return this.hasher(keys.join('###'), 31);
+      } else {
+        return this.murmurhash3_32_gc(keys.join('###'), 31);
+      }
+    },
+
+    /**
+     * JS Implementation of MurmurHash3 (r136) (as of May 20, 2011)
+     * 
+     * @author <a href="mailto:gary.court@gmail.com">Gary Court</a>
+     * @see http://github.com/garycourt/murmurhash-js
+     * @author <a href="mailto:aappleby@gmail.com">Austin Appleby</a>
+     * @see http://sites.google.com/site/murmurhash/
+     * 
+     * @param {string} key ASCII only
+     * @param {number} seed Positive integer only
+     * @return {number} 32-bit positive integer hash 
+     */
+
+    murmurhash3_32_gc: function murmurhash3_32_gc(key, seed) {
+      var remainder, bytes, h1, h1b, c1, c2, k1, i;
+
+      remainder = key.length & 3; // key.length % 4
+      bytes = key.length - remainder;
+      h1 = seed;
+      c1 = 0xcc9e2d51;
+      c2 = 0x1b873593;
+      i = 0;
+
+      while (i < bytes) {
+        k1 = key.charCodeAt(i) & 0xff | (key.charCodeAt(++i) & 0xff) << 8 | (key.charCodeAt(++i) & 0xff) << 16 | (key.charCodeAt(++i) & 0xff) << 24;
+        ++i;
+
+        k1 = (k1 & 0xffff) * c1 + (((k1 >>> 16) * c1 & 0xffff) << 16) & 0xffffffff;
+        k1 = k1 << 15 | k1 >>> 17;
+        k1 = (k1 & 0xffff) * c2 + (((k1 >>> 16) * c2 & 0xffff) << 16) & 0xffffffff;
+
+        h1 ^= k1;
+        h1 = h1 << 13 | h1 >>> 19;
+        h1b = (h1 & 0xffff) * 5 + (((h1 >>> 16) * 5 & 0xffff) << 16) & 0xffffffff;
+        h1 = (h1b & 0xffff) + 0x6b64 + (((h1b >>> 16) + 0xe654 & 0xffff) << 16);
+      }
+
+      k1 = 0;
+
+      switch (remainder) {
+        case 3:
+          k1 ^= (key.charCodeAt(i + 2) & 0xff) << 16;
+        case 2:
+          k1 ^= (key.charCodeAt(i + 1) & 0xff) << 8;
+        case 1:
+          k1 ^= key.charCodeAt(i) & 0xff;
+
+          k1 = (k1 & 0xffff) * c1 + (((k1 >>> 16) * c1 & 0xffff) << 16) & 0xffffffff;
+          k1 = k1 << 15 | k1 >>> 17;
+          k1 = (k1 & 0xffff) * c2 + (((k1 >>> 16) * c2 & 0xffff) << 16) & 0xffffffff;
+          h1 ^= k1;
+      }
+
+      h1 ^= key.length;
+
+      h1 ^= h1 >>> 16;
+      h1 = (h1 & 0xffff) * 0x85ebca6b + (((h1 >>> 16) * 0x85ebca6b & 0xffff) << 16) & 0xffffffff;
+      h1 ^= h1 >>> 13;
+      h1 = (h1 & 0xffff) * 0xc2b2ae35 + (((h1 >>> 16) * 0xc2b2ae35 & 0xffff) << 16) & 0xffffffff;
+      h1 ^= h1 >>> 16;
+
+      return h1 >>> 0;
+    },
+
+    // https://bugzilla.mozilla.org/show_bug.cgi?id=781447
+    hasLocalStorage: function hasLocalStorage() {
+      try {
+        return !!window.localStorage;
+      } catch (e) {
+        return true; // SecurityError when referencing it means it exists
+      }
+    },
+
+    hasSessionStorage: function hasSessionStorage() {
+      try {
+        return !!window.sessionStorage;
+      } catch (e) {
+        return true; // SecurityError when referencing it means it exists
+      }
+    },
+
+    isCanvasSupported: function isCanvasSupported() {
+      var elem = document.createElement('canvas');
+      return !!(elem.getContext && elem.getContext('2d'));
+    },
+
+    isIE: function isIE() {
+      if (navigator.appName === 'Microsoft Internet Explorer') {
+        return true;
+      } else if (navigator.appName === 'Netscape' && /Trident/.test(navigator.userAgent)) {
+        // IE 11
+        return true;
+      }
+      return false;
+    },
+
+    getPluginsString: function getPluginsString() {
+      if (this.isIE() && this.ie_activex) {
+        return this.getIEPluginsString();
+      } else {
+        return this.getRegularPluginsString();
+      }
+    },
+
+    getRegularPluginsString: function getRegularPluginsString() {
+      return this.map(navigator.plugins, function (p) {
+        var mimeTypes = this.map(p, function (mt) {
+          return [mt.type, mt.suffixes].join('~');
+        }).join(',');
+        return [p.name, p.description, mimeTypes].join('::');
+      }, this).join(';');
+    },
+
+    getIEPluginsString: function getIEPluginsString() {
+      if (window.ActiveXObject) {
+        var names = ['ShockwaveFlash.ShockwaveFlash', //flash plugin
+        'AcroPDF.PDF', // Adobe PDF reader 7+
+        'PDF.PdfCtrl', // Adobe PDF reader 6 and earlier, brrr
+        'QuickTime.QuickTime', // QuickTime
+        // 5 versions of real players
+        'rmocx.RealPlayer G2 Control', 'rmocx.RealPlayer G2 Control.1', 'RealPlayer.RealPlayer(tm) ActiveX Control (32-bit)', 'RealVideo.RealVideo(tm) ActiveX Control (32-bit)', 'RealPlayer', 'SWCtl.SWCtl', // ShockWave player
+        'WMPlayer.OCX', // Windows media player
+        'AgControl.AgControl', // Silverlight
+        'Skype.Detection'];
+
+        // starting to detect plugins in IE
+        return this.map(names, function (name) {
+          try {
+            new ActiveXObject(name);
+            return name;
+          } catch (e) {
+            return null;
+          }
+        }).join(';');
+      } else {
+        return ""; // behavior prior version 0.5.0, not breaking backwards compat.
+      }
+    },
+
+    getScreenResolution: function getScreenResolution() {
+      return [screen.height, screen.width];
+    },
+
+    getCanvasFingerprint: function getCanvasFingerprint() {
+      var canvas = document.createElement('canvas');
+      var ctx = canvas.getContext('2d');
+      // https://www.browserleaks.com/canvas#how-does-it-work
+      var txt = 'http://valve.github.io';
+      ctx.textBaseline = "top";
+      ctx.font = "14px 'Arial'";
+      ctx.textBaseline = "alphabetic";
+      ctx.fillStyle = "#f60";
+      ctx.fillRect(125, 1, 62, 20);
+      ctx.fillStyle = "#069";
+      ctx.fillText(txt, 2, 15);
+      ctx.fillStyle = "rgba(102, 204, 0, 0.7)";
+      ctx.fillText(txt, 4, 17);
+      return canvas.toDataURL();
+    }
+  };
+
+  return Fingerprint;
+});
 
 /***/ }),
 
