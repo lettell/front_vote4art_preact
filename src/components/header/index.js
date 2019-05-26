162ce6bec7d718cebddbdf0fdad0b38b55e7c934
lettell <lettell@localhost.localdomain>
2019-05-26 07:40:10 +0300

    build

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -158,7 +158,7 @@ export default class Header extends Component {
 						<TopAppBar.Section align-center>
 							<img onClick={this.goHome} class={style.logo} src="/assets/images/logo.png" />
 							{this.state.logined?
-							<Button onClick={this.goRead} unelevated >Balavau</Button>: ''}
+							<Button onClick={this.goRead} unelevated >Balsavau</Button>: ''}
 
 							<TopAppBar.Title></TopAppBar.Title>
 						</TopAppBar.Section>
